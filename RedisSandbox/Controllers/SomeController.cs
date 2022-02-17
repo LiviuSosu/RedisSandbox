9dbab35b6f0fe820ab2cc57580e4911326194a60
LiviuSosu <sosuliviu@gmail.com>
2022-02-17 18:11:59 +0100

    working demo

--- RedisSandbox/Controllers/SomeController.cs
+++ RedisSandbox/Controllers/SomeController.cs
@@ -1,33 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
 
 namespace RedisSandbox.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class SomeController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private readonly IDistributedCache _distributedCache;
 
-         private readonly IDistributedCache _distributedCache;
-
-        public WeatherForecastController(IDistributedCache distributedCache)
+        public SomeController(IDistributedCache distributedCache)
         {
             _distributedCache = distributedCache;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public void Get()
         {
             try
             {
                 //https://www.youtube.com/watch?v=188Fy-oCw4w&ab_channel=ProgrammingKnowledge2
                 //C:\Program Files\Redis
 
-                var cacheKey = "Livi";
+                var cacheKey = "MyRedisKEy";
                 var existingTime = _distributedCache.GetString(cacheKey);
                 if (!string.IsNullOrEmpty(existingTime))
                 {
@@ -40,18 +36,13 @@ namespace RedisSandbox.Controllers
                     var y = "Added to cache : " + existingTime;
                 }
             }
+            catch (RedisConnectionException ex)
+            {
+
+            }
             catch (Exception ex)
             {
-                throw new NotImplementedException();
             }
-
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
     }
 }
